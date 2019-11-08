db600853027e46707ff6fbdd0d1217197f18f8f1
Hemanth1437 <57118234+Hemanth1437@users.noreply.github.com>
2019-11-08 11:47:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.engine('handlebars', exphbs({defaultLayout:'main'}));
 app.get('/',routes.home);
 app.get('/:city',routes.city);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 var server=app.listen(port,function(req,res){
     console.log('Catch the action at http://localhost:'+port);
